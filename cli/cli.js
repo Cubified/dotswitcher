d1e2f0483db326e44302cd5954b9cce6b0fef596
CubeDoodl <andrewrussell2002@gmail.com>
2017-08-03 13:17:14 -0400

    Add a CLI, add package-lock, add git info to package.json

---
+++ cli/cli.js
@@ -0,0 +1,42 @@
+const fs = require('fs');
+const utils = require('../utils.js');
+
+if(!fs.existsSync(utils.dir)){
+	fs.mkdirSync(utils.dir);
+}
+if(!fs.existsSync(`${utils.dir}/configs`)){
+	fs.mkdirSync(`${utils.dir}/configs`);
+}
+if(!fs.existsSync(`${utils.dir}/list`)){
+	fs.writeFileSync(`${utils.dir}/list`,fs.readFileSync(`list.default`).toString());
+}
+
+let program = require('commander');
+
+program
+	.version('0.0.1-testing')
+	.option('-s, --save [name]', 'save the current dotfile configuration')
+	.option('-l, --load [name]', 'load a saved dotfile configuration')
+	.option('-v, --view', 'view the whitelist/blacklist')
+	.parse(process.argv);
+
+if(!program.save && !program.load && !program.view){
+	program.help();
+}
+
+if(typeof program.save === 'string'){
+	utils.save(program.save);
+} else if(!program.save === undefined){
+	program.help();
+}
+
+if(typeof program.load === 'string'){
+	utils.load(program.load);
+} else if(!program.load === undefined){
+	program.help();
+}
+
+if(program.view){
+	console.log(utils.type?'Whitelist:':'Blacklist:');
+	console.log(utils.whitelist().join('\n'));
+}
